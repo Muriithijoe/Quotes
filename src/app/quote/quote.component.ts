de3503a81adf20002254b01d57e282023a8bd7bb
Joe <joeace2000@gmail.comgit config --global user.name Muriithijoe>
2018-09-17 18:01:32 +0300

    styled page again

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -25,6 +25,16 @@ export class QuoteComponent implements OnInit {
     this.quotes[index].dislike += 1;
   }
 
+   deleteQuote (isComplete,index) {
+     if (isComplete){
+       let toDelete=confirm ('Are sure you want to delete ${this.quotes[index].name}')
+
+       if(toDelete){
+         this.quotes.splice(index,1)
+       }
+     }
+  }
+
 
 
   constructor() { }
