c22f9640c69ff4bafe973334b20f4c897ef29403
Joe <joeace2000@gmail.comgit config --global user.name Muriithijoe>
2018-09-18 16:46:54 +0300

    change of quotes

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -11,6 +11,7 @@ export class QuoteComponent implements OnInit {
   quotes = [
     new Quote('Arya Stark', 'Leave one wolf alive the sheep are never safe', 'George kim', 0, 0),
     new Quote('Tyrion Lannister', 'A mind needs books like a sword needs a whetstone', 'GOT enthusiast', 0, 0 ),
+    new Quote('Joffrey Baratheon', 'Paint stripes on a toad do not make a tiger', 'J.M.Wambugu', 0, 0 )
   ]
 
   addNewQuote(quote){
