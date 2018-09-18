fae876f4cdf7592e7a2642e6980b37dfe0319098
Joe <joeace2000@gmail.comgit config --global user.name Muriithijoe>
2018-09-18 08:07:56 +0300

    Change of quotes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,16 @@
-import { Component } from '@angular/core';
-import {Quote} from './quote'
+// import { Component } from '@angular/core';
+// import {Quote} from './quote'
 
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
-})
-export class AppComponent {
-  quotes = [
-    new Quote('Arya Stark', 'Leave one wolf alive the sheep are never safe', 'George kim', 0, 0),
-    new Quote('Tyrion Lannister', 'A mind needs books like a sword needs a whetstone', 'GOT enthusiast', 0, 0 ),
-  ]
+// @Component({
+//   selector: 'app-root',
+//   templateUrl: './app.component.html',
+//   styleUrls: ['./app.component.css']
+// })
+// export class AppComponent {
+//   quotes = [
+//     new Quote('Arya Stark', 'Leave one wolf alive the sheep are never safe', 'George kim', 0, 0),
+//     new Quote('Tyrion Lannister', 'A mind needs books like a sword needs a whetstone', 'GOT enthusiast', 0, 0 ),
+//   ]
 
   
-}
+// }
